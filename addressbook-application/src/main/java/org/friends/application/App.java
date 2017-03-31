cc6938f8d87188acddd1112659dae046c1393ca1
Darek <newsecond@outlook.com>
2017-03-31 12:34:57 +0200

    add maven modules

---
+++ addressbook-application/src/main/java/org/friends/application/App.java
@@ -0,0 +1,13 @@
+package org.friends.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
